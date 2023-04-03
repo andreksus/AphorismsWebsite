99bde69e5ccf2bc4a00f3f671dd71b17d5741041
Андрей Барышев <baryshev_2002@inbox.ru>
2023-04-03 08:32:50 +0600

    Added quotes data

--- public/data/Quotes.ts
+++ public/data/Quotes.ts
@@ -10,11 +10,93 @@ export const quotes: IQuote[] = [
     },
 
     {
-
+        category: ["writer"],
+        quote: "I thought how unpleasant it is to be locked out; and I thought how it is worse, perhaps, to be locked in.",
+        author: "Virginia Woolf",
+        favorite: false,
+        likes: 45,
+            image: "https://zenquotes.io/img/virginia-woolf.jpg",
     },
 
     {
+        category: ["writer"],
+        quote: "Freedom lies in being bold.",
+        author: "Robert Frost",
+        favorite: false,
+        likes: 70,
+        image: "https://zenquotes.io/img/robert-frost.jpg",
+    },
 
+    {
+        category: ["philosopher"],
+        quote: "Muddy water is best cleared by leaving it alone.",
+        author: "Alan Watts",
+        favorite: false,
+        likes: 13,
+        image: "https://zenquotes.io/img/alan-watts.jpg",
+    },
+
+    {
+        category: ["writer", "psychologist"],
+        quote: "Trusting our intuition often saves us from disaster.",
+        author: "Anne Wilson Schaef",
+        favorite: false,
+        likes: 33,
+        image: "https://zenquotes.io/img/anne-wilson-schaef.jpg",
+    },
+
+    {
+        category: ["composer"],
+        quote: "Better to die fighting for freedom than be a prisoner all the days of your life.",
+        author: "Bob Marley",
+        favorite: false,
+        likes: 89,
+        image: "https://zenquotes.io/img/bob-marley.jpg",
+    },
+
+    {
+        category: ["monk"],
+        quote: "Your work is to discover your work and then, with all your heart, to give yourself to it.",
+        author: "Buddha",
+        favorite: false,
+        likes: 92,
+        image: "https://zenquotes.io/img/buddha.jpg",
+    },
+
+    {
+        category: ["monk"],
+        quote: "Remember that sometimes not getting what you want is a wonderful stroke of luck.",
+        author: "Dalai Lama",
+        favorite: false,
+        likes: 56,
+        image: "https://zenquotes.io/img/dalai-lama.jpg",
+    },
+
+    {
+        category: ["magician"],
+        quote: "It does not do to dwell on dreams and forget to live, remember that.",
+        author: "Albus Dumbledore",
+        favorite: false,
+        likes: 111,
+        image: "https://zenquotes.io/img/albus-dumbledore.jpg",
+    },
+
+    {
+        category: ["physicist"],
+        quote: "In the middle of difficulty lies opportunity.",
+        author: "Albert Einstein",
+        favorite: false,
+        likes: 62,
+        image: "https://zenquotes.io/img/albert-einstein.jpg",
+    },
+
+    {
+        category: ["writer"],
+        quote: "It doesn't matter where you are coming from. All that matters is where you are going.",
+        author: "Brian Tracy",
+        favorite: false,
+        likes: 77,
+        image: "https://zenquotes.io/img/brian-tracy.jpg",
     },
 
 ]
